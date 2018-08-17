41a151938ea40ccd9ec1ac407e5b83fe0fd45fde
Kacper Drapała <kacdrapala@gmail.com>
2018-08-17 22:41:48 +0200

    Word count

--- word-count/WordCount.cs
+++ word-count/WordCount.cs
@@ -1,10 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class WordCount
 {
     public static IDictionary<string, int> CountWords(string phrase)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var editedPhrase = Regex.Replace(phrase.ToLower(), @"\s\'(.*?)\'\s", " $1 ") //replace quotations to without
+            .Split(new char[] { ' ', ',', '\n', ':', '.', '!' }, StringSplitOptions.RemoveEmptyEntries) // split by every characters and remove empty strings
+            .Where(q => q.ToArray().Any(Char.IsLetterOrDigit)); // get only this where strings is letter or digits
+
+        return editedPhrase.GroupBy(q => q).ToDictionary(w => w.Key, w => w.Count()); //convert Ienumerable to dictionary where key is each word and value is a count
     }
 }
